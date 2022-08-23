872287a37acff1fa24189d360ff913b71b402a00
寺下大典 <ekusiad.tera@icloud.com>
2022-08-23 20:39:36 +0900

    ドラッグ時のスタイル適用

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import dummyData from '../dummyData'
+import Card from './Card';
 
 function Main() {
+    const [data,setData] = useState(dummyData);
+
   return (
-    <div>Main</div>
+    <DragDropContext>
+        <div className='trello'>
+            {data.map(section => (
+                <Droppable key={section.id} droppableId={section.id}>
+                    {(provided) => (<div className="trello-section" ref={provided.innerRef} {...provided.droppableProps}>
+                            <div className='trello-section-title'>{section.title}</div>
+                            <div className='trello-section-content'>
+                                {section.tasks.map((task,index) => (
+                                    <Draggable draggableId={task.id} index={index} key={task.id} >
+                                        {(provided,snapshot) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                style={{
+                                                    ...provided.draggableProps.style,
+                                                    opacity: snapshot.isDragging ? '0.3' : '1',
+                                                }}
+                                            >
+                                                <Card></Card>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                            </div>
+                        </div>
+                        )}
+                </Droppable>
+            ))}
+        </div>
+    </DragDropContext>
   )
 }
 
